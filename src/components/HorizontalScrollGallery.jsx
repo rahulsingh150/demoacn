eee6f023bb91ee0b188809d0b6eee10fcea75cc0
rahulsingh150 <rahulsingh22343@gmail.com>
2024-06-27 13:38:33 +0530

    carriers,visitors

--- src/components/HorizontalScrollGallery.jsx
+++ src/components/HorizontalScrollGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import news1 from '../Assests/Media/1.jpg';
 import news2 from '../Assests/Media/2.jpg';
 import news3 from '../Assests/Media/3.jpg';
@@ -10,113 +10,81 @@ import news8 from '../Assests/Media/8.jpeg';
 import '../Styles/HorizontalScrollGallery.css';
 
 const images = [
-  { src: news1, year: '2024' },
-  { src: news1, year: '2024' },
-  { src: news1, year: '2024' },
-  { src: news2, year: '2024' },
-  { src: news3, year: '2024' },
-  { src: news4, year: '2024' },
-  { src: news5, year: '2023' },
-  { src: news6, year: '2023' },
-  { src: news6, year: '2023' },
-  { src: news6, year: '2023' },
-  { src: news7, year: '2023' },
-  { src: news8, year: '2023' },
+  { src: news1 },
+  { src: news1 },
+  { src: news1 },
+  { src: news2 },
+  { src: news3 },
+  { src: news4 },
+  { src: news5 },
+  { src: news6 },
+  { src: news6 },
+  { src: news6 },
+  { src: news7 },
+  { src: news8 },
 ];
 
-const groupImagesByYear = (images) => {
-  return images.reduce((acc, image) => {
-    const year = image.year;
-    if (!acc[year]) {
-      acc[year] = [];
-    }
-    acc[year].push(image);
-    return acc;
-  }, {});
-};
-
 const HorizontalScrollGallery = () => {
-  const [isHovering, setIsHovering] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
-  const scrollRefs = useRef({});
 
-  const groupedImages = groupImagesByYear(images);
-
-  useEffect(() => {
-    const scrollIntervals = {};
-
-    Object.keys(groupedImages).forEach((year) => {
-      const scrollContainer = scrollRefs.current[year];
-      if (scrollContainer) {
-        const startScrolling = () => {
-          if (scrollIntervals[year]) {
-            clearInterval(scrollIntervals[year]);
-          }
-          scrollIntervals[year] = setInterval(() => {
-            scrollContainer.scrollLeft += 2;
-          }, 30);
-        };
-
-        // startScrolling();
-        if (!isHovering) {
-          startScrolling();
-        }
-
-        scrollContainer.addEventListener('mouseenter', () => {
-          clearInterval(scrollIntervals[year]);
-        });
-
-        scrollContainer.addEventListener('mouseleave', startScrolling);
-      }
-    });
-
-    return () => {
-      Object.keys(scrollIntervals).forEach((year) => {
-        clearInterval(scrollIntervals[year]);
-      });
-    };
-  }, []);
-
-  const handleMouseEnter = () => setIsHovering(true);
-  const handleMouseLeave = () => setIsHovering(false);
   const handleImageClick = (image) => {
     setSelectedImage(image);
     setIsModalOpen(true);
   };
-  const handleCloseModal = () => setIsModalOpen(false);
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
 
   return (
-    <div className="media-center">
-    <h1 style={{textAlign: 'center'}}>Media Center</h1>
-      {Object.keys(groupedImages).map((year) => (
-        <div key={year} className="year-group">
-          <div className="year-header">
-            <span className="year">{year}</span>
-          </div>
-          <div
-            className="scroll-container"
-            ref={(el) => (scrollRefs.current[year] = el)}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            {groupedImages[year].map((image, index) => (
-              <div key={index} className="image-container">
-                <img
-                  src={image.src}
-                  alt={`image-${index}`}
-                  className="scroll-image"
-                  onClick={() => handleImageClick(image.src)}
-                />
-              </div>
-            ))}
-          </div>
+    <div style={{ margin: '40px' }}>
+      <h2 style={{ fontWeight: 'bold' }}>Media Center</h2>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div
+          style={{
+            width: '150px',
+            height: '50px',
+            backgroundColor: 'rgb(120, 0, 0)',
+            overflow: 'hidden',
+            color: 'white',
+            fontSize: '35px',
+            textAlign: 'left',
+            paddingLeft: '25px',
+            paddingBottom: '18px',
+          }}
+        >
+          2024
         </div>
-      ))}
+        <div
+          style={{
+            width: '0px',
+            height: '0px',
+            borderTop: '50px solid transparent',
+            borderLeft: '50px solid rgb(120, 0, 0)',
+            paddingBottom: '1px',
+          }}
+        ></div>
+      </div>
+      <div className="autoslider">
+        <div className="slide-track">
+          {images.map((image, index) => (
+            <div className="aslide" key={index}>
+              <img
+                src={image.src}
+                alt={`Image ${index + 1}`}
+                onClick={() => handleImageClick(image.src)}
+                style={{ cursor: 'pointer' }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
 
       {isModalOpen && (
         <div className="modal-overlay" onClick={handleCloseModal}>
-          <div className="modal-content">
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <img src={selectedImage} alt="Selected" className="modal-image" />
             <button onClick={handleCloseModal} className="close-button">
               Close
