557b0a739cbea7bfcfa35c4e1b86f622488751d2
rahulsingh150 <rahulsingh22343@gmail.com>
2024-06-27 14:56:50 +0530

    visitors

--- src/components/HorizontalScrollGallery.jsx
+++ src/components/HorizontalScrollGallery.jsx
@@ -1,27 +1,132 @@
 import React, { useState } from 'react';
-import news1 from '../Assests/Media/1.jpg';
-import news2 from '../Assests/Media/2.jpg';
-import news3 from '../Assests/Media/3.jpg';
-import news4 from '../Assests/Media/4.jpg';
-import news5 from '../Assests/Media/5.jpg';
-import news6 from '../Assests/Media/6.jpeg';
-import news7 from '../Assests/Media/7.jpeg';
-import news8 from '../Assests/Media/8.jpeg';
+
+import second1 from '../Assests/news photo/2023/1.jpeg'
+import second2 from '../Assests/news photo/2023/asset 1.jpeg'
+import second3 from '../Assests/news photo/2023/asset 10.jpeg'
+import second4 from '../Assests/news photo/2023/asset 12.jpeg'
+import second5 from '../Assests/news photo/2023/asset 13.jpeg'
+import second6 from '../Assests/news photo/2023/asset 14.jpeg'
+import second7 from '../Assests/news photo/2023/asset 15.jpeg'
+import second8 from '../Assests/news photo/2023/asset 16.jpeg'
+import second9 from '../Assests/news photo/2023/asset 17.jpeg'
+import second10 from '../Assests/news photo/2023/asset 18.jpeg'
+import second11 from '../Assests/news photo/2023/asset 19.jpeg'
+import second12 from '../Assests/news photo/2023/asset 2.jpeg'
+import second13 from '../Assests/news photo/2023/asset 20.jpeg'
+import second14 from '../Assests/news photo/2023/asset 21.jpeg'
+import second15 from '../Assests/news photo/2023/asset 22.jpeg'
+import second16 from '../Assests/news photo/2023/asset 23.jpeg'
+import second17 from '../Assests/news photo/2023/asset 24.jpeg'
+import second18 from '../Assests/news photo/2023/asset 26.jpeg'
+import second19 from '../Assests/news photo/2023/asset 27.jpeg'
+import second20 from '../Assests/news photo/2023/asset 28.jpeg'
+import second21 from '../Assests/news photo/2023/asset 29.jpeg'
+import second22 from '../Assests/news photo/2023/asset 3.jpeg'
+import second23 from '../Assests/news photo/2023/asset 30.jpeg'
+import second24 from '../Assests/news photo/2023/asset 31.jpeg'
+import second25 from '../Assests/news photo/2023/asset 33.jpeg'
+import second26 from '../Assests/news photo/2023/asset 34.jpeg'
+import second27 from '../Assests/news photo/2023/asset 35.jpeg'
+import second28 from '../Assests/news photo/2023/asset 36.jpeg'
+import second29 from '../Assests/news photo/2023/asset 37.jpeg'
+import second30 from '../Assests/news photo/2023/asset 38.jpeg'
+import second31 from '../Assests/news photo/2023/asset 39.jpeg'
+import second32 from '../Assests/news photo/2023/asset 4.jpeg'
+import second33 from '../Assests/news photo/2023/asset 41.jpeg'
+import second34 from '../Assests/news photo/2023/asset 42.jpeg'
+import second35 from '../Assests/news photo/2023/asset 43.jpeg'
+import second36 from '../Assests/news photo/2023/asset 44.jpeg'
+import second37 from '../Assests/news photo/2023/asset 45.jpeg'
+import second38 from '../Assests/news photo/2023/asset 46.jpeg'
+import second39 from '../Assests/news photo/2023/asset 47.jpeg'
+import second40 from '../Assests/news photo/2023/asset 48.jpeg'
+import second41 from '../Assests/news photo/2023/asset 49.jpeg'
+import second42 from '../Assests/news photo/2023/asset 5.jpeg'
+import second43 from '../Assests/news photo/2023/asset 50.jpeg'
+import second44 from '../Assests/news photo/2023/asset 51.jpeg'
+import second45 from '../Assests/news photo/2023/asset 52.jpeg'
+import second46 from '../Assests/news photo/2023/asset 53.jpeg'
+import second47 from '../Assests/news photo/2023/asset 56.jpeg'
+import second48 from '../Assests/news photo/2023/asset 57.jpeg'
+import second49 from '../Assests/news photo/2023/asset 58.jpeg'
+import second50 from '../Assests/news photo/2023/asset 59.jpeg'
+import second51 from '../Assests/news photo/2023/asset 60.jpeg'
+import second52 from '../Assests/news photo/2023/asset 61.jpeg'
+import second53 from '../Assests/news photo/2023/asset 62.jpeg'
+import second54 from '../Assests/news photo/2023/asset 64.jpeg'
+import second55 from '../Assests/news photo/2023/asset 65.jpeg'
+
+import frist1 from '../Assests/news photo/2024/asset 11.jpeg'
+import frist2 from '../Assests/news photo/2024/asset 32.jpeg'
+import frist3 from '../Assests/news photo/2024/asset 54.jpeg'
+import frist4 from '../Assests/news photo/2024/asset 55.jpeg'
+import frist5 from '../Assests/news photo/2024/asset 9.jpeg'
 import '../Styles/HorizontalScrollGallery.css';
 
+const image24 = [
+  { src: frist1 },
+  { src: frist2 },
+  { src: frist3 },
+  { src: frist4 },
+  { src: frist5 },
+  { src: second26 },
+  { src: second27 },
+  { src: second28 },
+  { src: second29 },
+  { src: second30 },
+  { src: second31 },
+  { src: second32 },
+  { src: second33 },
+  { src: second34 },
+  { src: second35 },
+  { src: second36 },
+  { src: second37 },
+  { src: second38 },
+  { src: second39 },
+  { src: second40 },
+  { src: second41 },
+  { src: second42 },
+  { src: second43 },
+  { src: second44 },
+  { src: second45 },
+  { src: second46 },
+  { src: second47 },
+  { src: second48 },
+  { src: second49 },
+  { src: second50 },
+  { src: second51 },
+  { src: second52 },
+  { src: second53 },
+  { src: second54 },
+  { src: second55 },
+]
+
 const images = [
-  { src: news1 },
-  { src: news1 },
-  { src: news1 },
-  { src: news2 },
-  { src: news3 },
-  { src: news4 },
-  { src: news5 },
-  { src: news6 },
-  { src: news6 },
-  { src: news6 },
-  { src: news7 },
-  { src: news8 },
+  { src: second1 },
+  { src: second2 },
+  { src: second3 },
+  { src: second4 },
+  { src: second5 },
+  { src: second6 },
+  { src: second7 },
+  { src: second8 },
+  { src: second9 },
+  { src: second10 },
+  { src: second11 },
+  { src: second12 },
+  { src: second13 },
+  { src: second14 },
+  { src: second15 },
+  { src: second16 },
+  { src: second17 },
+  { src: second18 },
+  { src: second19 },
+  { src: second20 },
+  { src: second21 },
+  { src: second22 },
+  { src: second23 },
+  { src: second24 },
+  { src: second25 },
 ];
 
 const HorizontalScrollGallery = () => {
@@ -39,8 +144,8 @@ const HorizontalScrollGallery = () => {
   };
 
   return (
-    <div style={{ margin: '40px' }}>
-      <h2 style={{ fontWeight: 'bold' }}>Media Center</h2>
+    <div style={{ margin: '40px' ,background: '#f0e9e9'}}>
+      <h2 style={{ fontWeight: 'bold' ,textAlign: "center" , padding: '10px'}} className='H2-Heading'>Media Center</h2>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div
           style={{
@@ -70,6 +175,49 @@ const HorizontalScrollGallery = () => {
       <div className="autoslider">
         <div className="slide-track">
           {images.map((image, index) => (
+            <div className="aslide" key={index}>
+              <img
+                src={image.src}
+                alt={`Image ${index + 1}`}
+                onClick={() => handleImageClick(image.src)}
+                style={{ cursor: 'pointer' }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div
+          style={{
+            width: '150px',
+            height: '50px',
+            backgroundColor: 'rgb(120, 0, 0)',
+            overflow: 'hidden',
+            color: 'white',
+            fontSize: '35px',
+            textAlign: 'left',
+            paddingLeft: '25px',
+            paddingBottom: '18px',
+          }}
+        >
+          2023
+        </div>
+        <div
+          style={{
+            width: '0px',
+            height: '0px',
+            borderTop: '50px solid transparent',
+            borderLeft: '50px solid rgb(120, 0, 0)',
+            paddingBottom: '1px',
+          }}
+        ></div>
+      </div>
+
+
+      <div className="autoslider">
+        <div className="slide-track">
+          {image24.map((image, index) => (
             <div className="aslide" key={index}>
               <img
                 src={image.src}
--- src/components/Institute.jsx
+++ src/components/Institute.jsx
@@ -10,12 +10,12 @@ const NewsAndAnnouncement = [
   {
     id: 0,
     NewsName: "Admission 2024",
-    href: 'https://adms.akubihar.ac.in/',
+    href: "https://adms.akubihar.ac.in/",
   },
   {
     id: 1,
     NewsName: "SNN Prospectus 2024",
-    href: '/PROSPECTUS2024.pdf',
+    href: "/PROSPECTUS2024.pdf",
   },
   {
     id: 2,
@@ -46,51 +46,86 @@ function Institute() {
         </div>
         <div className="Institute-tab-two">
           <div>
-            <Link to="/horizontalScrollGallery" style={{ textDecoration: "none" }}>
-              <div style={{
-                width: '340px',
-                height: '70px',
-                padding: '10px',
-                backgroundColor: 'rgb(240, 240, 240)',
-                margin: '10px',
-                position: 'relative',
-                bottom: '5px',
-                display: 'flex'
-              }} className="Meadia-res">
-                <div style={{
-                  backgroundColor: 'rgb(120, 0, 0)',
-                  borderRadius: '50%',
-                  width: '50px',
-                  height: '98%'
-                }}>
-                  <img
-                    src="https://sjmc.ac.in/static/media/media_center_logo.cf1cb1d00c99c1405854.png"
-                    style={{ width: '100%', height: 'auto' }}
-                    alt="Media Center Logo"
-                  />
+            <div className="Meadia-box"
+            >
+              <Link
+                to="/horizontalScrollGallery"
+                style={{ textDecoration: "none" }}
+              >
+                <div
+                  style={{
+                    padding: "15px",
+                    margin: "10px",
+                    position: "relative",
+                    bottom: "5px",
+                    display: "flex",
+                  }}
+                  className="Meadia-res"
+                >
+                  <div
+                    style={{
+                      backgroundColor: "rgb(120, 0, 0)",
+                      borderRadius: "50%",
+                      width: "50px",
+                      height: "98%",
+                    }}
+                  >
+                    <img
+                      src="https://sjmc.ac.in/static/media/media_center_logo.cf1cb1d00c99c1405854.png"
+                      style={{ width: "100%", height: "auto" }}
+                      alt="Media Center Logo"
+                    />
+                  </div>
+                  <div style={{ marginLeft: "15px", marginTop: "10px" }}>
+                    <h4>Media Center</h4>
+                  </div>
                 </div>
-                <div style={{ marginLeft: '15px', marginTop: '10px' }}>
-                  <h4>Media Center</h4>
-                </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
             <ImportantBox
               NewsAndAnnouncement={ImportantLinks}
               Image={Image}
               BoxHeading={BoxHeadingSecond}
             />
-
-
           </div>
 
-
-          <div className="Institute-tab-content" style={{ marginLeft: '10px' }}>
+          <div className="Institute-tab-content" style={{ marginLeft: "10px" }}>
             <h3 style={{ textAlign: "justify" }}>
-              Aryabhatta Center for Nanoscience & Technology(ACNN) is non-traditional, super specialized, frontier areas of subject of 21st century& first cutting edge Research Centre of university of Bihar, equipped with 20 high-end research instruments such as Scanning Electron Microscope, Multiferroic system, Vibrating Sample magnetometer, High energy ball mill etc. This is only one such frontiers related academic center in universities of Bihar. The Nanoscience and nanotechnology center of Aryabhatta Knowledge University was Established by the founder Vice Chancellor Prof. S N Guha with whole hearted support and encouragement from Honorable Chief minister Sri Nitish Kumar Ji. The first academic session of M.Tech and Ph.D. started from academic from year 2013. The academic and research activities of this Center visited/appreciated by Hon’ble Governor cum Chancellor Universities of Bihar, Hon’ble Chief minister Sri Nitish Kumar Ji, Education ministers ,various Govt. officers and eminent academicians of national/International repute.
+              Aryabhatta Center for Nanoscience & Technology(ACNN) is
+              non-traditional, super specialized, frontier areas of subject of
+              21st century& first cutting edge Research Centre of university of
+              Bihar, equipped with 20 high-end research instruments such as
+              Scanning Electron Microscope, Multiferroic system, Vibrating
+              Sample magnetometer, High energy ball mill etc. This is only one
+              such frontiers related academic center in universities of Bihar.
+              The Nanoscience and nanotechnology center of Aryabhatta Knowledge
+              University was Established by the founder Vice Chancellor Prof. S
+              N Guha with whole hearted support and encouragement from Honorable
+              Chief minister Sri Nitish Kumar Ji. The first academic session of
+              M.Tech and Ph.D. started from academic from year 2013. The
+              academic and research activities of this Center
+              visited/appreciated by Hon’ble Governor cum Chancellor
+              Universities of Bihar, Hon’ble Chief minister Sri Nitish Kumar Ji,
+              Education ministers ,various Govt. officers and eminent
+              academicians of national/International repute.
             </h3>
             <br />
             <h3 style={{ textAlign: "justify" }}>
-              Different affairs of academic, research &development programme are being carried out under the leadership of Dr. Rakesh Kumar Singh, head of the Nanoscience center(i/c) , who have been also awarded “ Best Young Teacher with Contributions in modern field of Nanoscience” by Hon’ble Chancellor of university of Bihar cum Governor for his outstanding performance . In the last 10 year more than 1000 eminent academicians/Scholars from different parts of world/country/state visited Nanoscience center of Aryabhatta Knowledge University. About more than 200 research papers published/ final progress in SCI/Scopus/WoS/Peer reviewed Journals by Nanotechnology center in multidisciplinary area of Science, Engineering & technology including Engineering Science, Agriculture, Electronics, Medicine, Food, and Ayurveda and Basic Sciences. 2 patents and 2 prototype have been filed/developed in the field of low cost LED, Agriculture and purification of water.
+              Different affairs of academic, research &development programme are
+              being carried out under the leadership of Dr. Rakesh Kumar Singh,
+              head of the Nanoscience center(i/c) , who have been also awarded “
+              Best Young Teacher with Contributions in modern field of
+              Nanoscience” by Hon’ble Chancellor of university of Bihar cum
+              Governor for his outstanding performance . In the last 10 year
+              more than 1000 eminent academicians/Scholars from different parts
+              of world/country/state visited Nanoscience center of Aryabhatta
+              Knowledge University. About more than 200 research papers
+              published/ final progress in SCI/Scopus/WoS/Peer reviewed Journals
+              by Nanotechnology center in multidisciplinary area of Science,
+              Engineering & technology including Engineering Science,
+              Agriculture, Electronics, Medicine, Food, and Ayurveda and Basic
+              Sciences. 2 patents and 2 prototype have been filed/developed in
+              the field of low cost LED, Agriculture and purification of water.
             </h3>
           </div>
         </div>
